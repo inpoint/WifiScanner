397c0a770ed7aaade40484c6d0884fc3e27981cc
chen.guo <guochen110@gmail.com>
2012-12-01 10:16:30 +0200

    old version~

--- src/com/example/wifiscanner/WifiScanner.java
+++ src/com/example/wifiscanner/WifiScanner.java
@@ -21,7 +21,6 @@ import android.net.wifi.WifiInfo;
 import android.net.wifi.WifiManager;
 import android.os.Bundle;
 import android.os.Environment;
-import android.telephony.TelephonyManager;
 import android.util.Log;
 import android.view.View;
 import android.view.View.OnClickListener;
@@ -32,8 +31,22 @@ import android.widget.Toast;
 import java.util.ArrayList;
 import android.provider.Settings.Secure;
 
-import android.provider.Settings;
-import android.provider.Settings.Secure;
+// Save data into XML//
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import org.xmlpull.v1.XmlSerializer;
+import android.app.Activity;
+import android.os.Bundle;
+import android.os.Environment;
+import android.util.Log;
+import android.util.Xml;
+import android.widget.TextView;
+
+//////////////////////
+
+
 
 public class WifiScanner extends Activity implements OnClickListener {
 	private static final String TAG = "WifiScanner";
@@ -104,9 +117,114 @@ public class WifiScanner extends Activity implements OnClickListener {
 					Toast.LENGTH_LONG).show();
 		}
 
-		textStatus = (TextView) findViewById(R.id.textStatus);
-		textStatus.setText("");
-		textStatus.append(UserID);
+        //create a new file called "new.xml" in the SD card
+        File newxmlfile = new File(Environment.getExternalStorageDirectory()+"/new.xml");
+        try{
+                newxmlfile.createNewFile();
+        }catch(IOException e){
+                Log.e("IOException", "exception in createNewFile() method");
+        }	
+        //we have to bind the new file with a FileOutputStream
+        FileOutputStream fileos = null;         
+        try{
+                fileos = new FileOutputStream(newxmlfile);
+        }catch(FileNotFoundException e){
+                Log.e("FileNotFoundException", "can't create FileOutputStream");
+        }
+        
+        //scan the wifi and save the result
+		Toast.makeText(this, "Start Scan now!!", Toast.LENGTH_LONG).show();
+		if (view.getId() == R.id.buttonScan) {
+			Log.d(TAG, "onClick() wifi.startScan()");
+
+		}
+        ArrayList<List<ScanResult>> ScanList = new ArrayList<List<ScanResult>>(5);
+        for (int scanCnt = 0; scanCnt < 6; scanCnt++)
+        {
+			wifi.startScan();
+			try {
+				Thread.sleep(1500);
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			List<ScanResult> results = this.wifi.getScanResults();
+			ScanList.add(results);
+//			ScanResult bestSignal = null;
+//			for (ScanResult result : results) {
+//				if (bestSignal == null
+//						|| WifiManager.compareSignalLevel(bestSignal.level,
+//								result.level) < 0)
+//					bestSignal = result;
+//			}
+        }
+        for (int i = 1; i < 5; i++) {
+        	if ScanList.get(1).
+        }
+        
+        
+        
+        
+        
+        //we create a XmlSerializer in order to write xml data
+        XmlSerializer serializer = Xml.newSerializer();
+        try {
+                //we set the FileOutputStream as output for the serializer, using UTF-8 encoding
+                        serializer.setOutput(fileos, "UTF-8");
+                        //Write <?xml declaration with encoding (if encoding not null) and standalone flag (if standalone not null) 
+                        serializer.startDocument(null, Boolean.valueOf(true)); 
+                        //set indentation option
+                        serializer.setFeature("http://xmlpull.org/v1/doc/features.html#indent-output", true); 
+                        //start a tag called "root"
+                        serializer.startTag(null, "root"); 
+                        // indent code just to have a view similar to xml-tree
+                                serializer.startTag(null, "AP1");
+                                serializer.attribute(null, "SSID", "value");
+                                serializer.attribute(null, "MacAddr", "value");
+                                serializer.attribute(null, "Sig", "value");
+                                serializer.endTag(null, "AP1");                          
+                                
+                                serializer.startTag(null, "AP2");
+                                serializer.attribute(null, "SSID", "value");
+                                serializer.attribute(null, "MacAddr", "value");
+                                serializer.attribute(null, "Sig", "value");
+                                serializer.endTag(null, "AP2");                     
+                                
+                                serializer.startTag(null, "AP3");
+                                serializer.attribute(null, "SSID", "value");
+                                serializer.attribute(null, "MacAddr", "value");
+                                serializer.attribute(null, "Sig", "value");
+                                serializer.endTag(null, "AP3");  
+                                
+                                serializer.startTag(null, "AP4");
+                                serializer.attribute(null, "SSID", "value");
+                                serializer.attribute(null, "MacAddr", "value");
+                                serializer.attribute(null, "Sig", "value");
+                                serializer.endTag(null, "AP4");
+                                 
+                                serializer.startTag(null, "AP5");
+                                serializer.attribute(null, "SSID", "value");
+                                serializer.attribute(null, "MacAddr", "value");
+                                serializer.attribute(null, "Sig", "value");
+                                serializer.endTag(null, "AP5");
+                                
+                        serializer.endTag(null, "root");
+                        serializer.endDocument();
+                        //write xml data into the FileOutputStream
+                        serializer.flush();
+                        //finally we close the file stream
+                        fileos.close();     
+                                
+//                TextView tv = (TextView)this.findViewById(R.id.result);
+//                        tv.setText("file has been created on SD card");
+                } catch (Exception e) {
+                	Log.e("Exception","error occurred while creating xml file");
+                }
+        
+		
+//		textStatus = (TextView) findViewById(R.id.textStatus);
+//		textStatus.setText("");
+//		textStatus.append(UserID);
 		Toast.makeText(this, "Start Scan now!!", Toast.LENGTH_LONG).show();
 		if (view.getId() == R.id.buttonScan) {
 			Log.d(TAG, "onClick() wifi.startScan()");
@@ -186,34 +304,70 @@ public class WifiScanner extends Activity implements OnClickListener {
 
 			Log.d(TAG, "onReceive() message: " + message);
 		}
-//		List<ScanResult> average=ScanList.get(0);
-//		for(int i=1;i<5;i++){
-//			List<ScanResult> res= ScanList.get(i);
-//			for(int j=0;j<res.size();j++){
-//				for(int z=0;z<average.size();i++)
-//					if(res.get(j).BSSID==average.get(z).BSSID){
-//						average.get(z).level+=res.get(j).level;
-//						break;
-//					}
-//			}
-//			textStatus.append("round" + i + ":"+res.size()+"first ap sig:" + res.get(1).level);
-//			
-//		}
-		/* get IMEI*/
-//		TelephonyManager telephonyManager = (TelephonyManager)getSystemService(Context.TELEPHONY_SERVICE);
-//		textStatus.append(telephonyManager.getDeviceId());
-		/* get Android ID*/
-		String Id = Settings.Secure.getString(getContentResolver(), Settings.Secure.ANDROID_ID);
-		textStatus.append(Id);
-		/* send AP info to the server through http*/
-		
+//		List<ScanResult> average;
 //		for(int i=0;i<5;i++){
 //			List<ScanResult> res= ScanList.get(i);
-//			for(int j=0;j<res.size();j++){
+//			for(int j=0;j<res.size();i++){
 //				
 //			}
-//			textStatus.append("round" + i + ":"+res.size()+"first ap sig:" + res.get(1).level);
 //			
 //		}
 	}
 }
+
+
+//public class XmlFileCreator extends Activity {
+//
+//    @Override
+//    public void onCreate(Bundle savedInstanceState) {
+//        super.onCreate(savedInstanceState);
+//        setContentView(R.layout.main);
+//        //create a new file called "new.xml" in the SD card
+//        File newxmlfile = new File(Environment.getExternalStorageDirectory()+"/new.xml");
+//        try{
+//                newxmlfile.createNewFile();
+//        }catch(IOException e){
+//                Log.e("IOException", "exception in createNewFile() method");
+//        }
+//        //we have to bind the new file with a FileOutputStream
+//        FileOutputStream fileos = null;         
+//        try{
+//                fileos = new FileOutputStream(newxmlfile);
+//        }catch(FileNotFoundException e){
+//                Log.e("FileNotFoundException", "can't create FileOutputStream");
+//        }
+//        //we create a XmlSerializer in order to write xml data
+//        XmlSerializer serializer = Xml.newSerializer();
+//        try {
+//                //we set the FileOutputStream as output for the serializer, using UTF-8 encoding
+//                        serializer.setOutput(fileos, "UTF-8");
+//                        //Write <?xml declaration with encoding (if encoding not null) and standalone flag (if standalone not null) 
+//                        serializer.startDocument(null, Boolean.valueOf(true)); 
+//                        //set indentation option
+//                        serializer.setFeature("http://xmlpull.org/v1/doc/features.html#indent-output", true); 
+//                        //start a tag called "root"
+//                        serializer.startTag(null, "root"); 
+//                        //i indent code just to have a view similar to xml-tree
+//                                serializer.startTag(null, "child1");
+//                                serializer.endTag(null, "child1");                          
+//                                serializer.startTag(null, "child2");
+//                                //set an attribute called "attribute" with a "value" for <child2>
+//                                serializer.attribute(null, "attribute", "value");
+//                                serializer.endTag(null, "child2");                     
+//                                serializer.startTag(null, "child3");
+//                                //write some text inside <child3>
+//                                serializer.text("some text inside child3");
+//                                serializer.endTag(null, "child3");                               
+//                        serializer.endTag(null, "root");
+//                        serializer.endDocument();
+//                        //write xml data into the FileOutputStream
+//                        serializer.flush();
+//                        //finally we close the file stream
+//                        fileos.close();                       
+//                TextView tv = (TextView)this.findViewById(R.id.result);
+//                        tv.setText("file has been created on SD card");
+//                } catch (Exception e) {
+//                        Log.e("Exception","error occurred while creating xml file");
+//                }
+//    }
+//}
